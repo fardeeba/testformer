e843f118e429ef6fab6558d71f2a3d1116a7874c
fardeeba <farah.deeba2805@gmail.com>
2023-09-22 00:23:28 +0500

    trial1

--- register_custom_data.py
+++ register_custom_data.py
@@ -11,6 +11,7 @@ CUSTOM_DATASET_CATEGORIES = [{"name": "NOT tumor", "id": 0, "trainId": 0},
 def _get_custom_dataset_meta():
     # Define your custom dataset categories and ids
     stuff_ids = [k["id"] for k in CUSTOM_DATASET_CATEGORIES]
+    assert len(stuff_ids) == 4, len(stuff_ids)
     
     stuff_dataset_id_to_contiguous_id = {k: i for i, k in enumerate(stuff_ids)}
     stuff_classes = [k["name"] for k in CUSTOM_DATASET_CATEGORIES]
@@ -23,18 +24,17 @@ def _get_custom_dataset_meta():
 
 def register_all_custom_dataset(root):
     meta = _get_custom_dataset_meta()
-    print("meta: ",meta)
     for name, dirname in [("train", "training"), ("val", "validation")]:  # Adjust dir names as per your dataset
         image_dir = os.path.join(root, "images", dirname)
-        gt_dir = os.path.join(root, "annotations", dirname)
+        gt_dir = os.path.join(root, "annotations_detectron2", dirname)
         name = f"BraTS20_{name}"
         DatasetCatalog.register(
-            name, lambda x=image_dir, y=gt_dir: load_sem_seg(y, x, gt_ext="tiff", image_ext="tiff")  # You can change the extensions
+            name, lambda x=image_dir, y=gt_dir: load_sem_seg(y, x, gt_ext="tif", image_ext="tif")  # You can change the extensions
         )
         MetadataCatalog.get(name).set(
             stuff_classes=meta["stuff_classes"][:],
             image_root=image_dir,
             sem_seg_root=gt_dir,
             evaluator_type="sem_seg",
-            ignore_label = 0
+            ignore_label = 65536,
         )
