a3793fe1b598f6577586efed3ca0b2c900fc9114
fardeeba <farah.deeba2805@gmail.com>
2023-09-21 14:29:44 +0500

    classes changed

--- train_net.py
+++ train_net.py
@@ -291,7 +291,7 @@ def setup(args):
     cfg.merge_from_list(args.opts)
     cfg.DATASETS.TRAIN = ("BraTS20_train",)
     cfg.DATASETS.TEST = ("BraTS20_val",)
-    cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES = 4
+    cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES = 3
     cfg.INPUT.CROP.SIZE = (128,128,)
     cfg.INPUT.MIN_SIZE_TEST = 128
     cfg.INPUT.COLOR_AUG_SSD = False
